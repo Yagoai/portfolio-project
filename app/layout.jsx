ad17edf9f602767ef00e12b418bfa3a144c81d4d
NguyenHongAnh <nguyenhonganh19052003@gmail.com>
2026-02-04 01:14:50 +0700

    update: add Transitions components

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ import "./globals.css";
 
 // Components
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono ({
   subsets: ["latin"], 
@@ -21,7 +23,11 @@ export default function RootLayout({ children }) {
       <body
         className={jetbrainsMono.variable}>
           <Header />
-          {children}
+          <StairTransition/>
+          <PageTransition>
+            {children}
+          </PageTransition>
+
           </body>
     </html>
   );
