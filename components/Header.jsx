e4ba5d1257b5723a17cd12c23f4f2537b1be7873
NguyenHongAnh <nguyenhonganh19052003@gmail.com>
2026-02-04 01:15:45 +0700

    update: add Navigation for desktop and mobile
    Implementing desktop nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,15 +7,25 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
+
         {/* Logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Anh<span className="text-accent">.</span>
           </h1>
         </Link>
-        {/* Desktop Nav */}
-        <Nav />
+
+        {/* Desktop Nav & Hire me Button*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav/>
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
