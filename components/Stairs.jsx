e2f9fcefd4e5c63feafd02eb206574ee09606daa
NguyenHongAnh <nguyenhonganh19052003@gmail.com>
2026-02-05 00:34:06 +0700

    update: update StairTransition

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,38 +1,55 @@
-import { animate, motion } from "framer-motion"
+import { animate, motion } from "framer-motion";
 
 // Varients
 const stairAnimation = {
-    initial: {
-        top: "0%",
+  initial: {
+    top: "0%",
+  },
 
-    },
+  animate: {
+    top: "100%",
+  },
 
-    animate: {
-        top: "100%",
-    },
-
-    exit: {
-        top: ["100%", "0%"],
-    },
-}
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
 
 // Calculated the reverse index fot staggered delay
-const  reverseIndex = (index) => {
-    // Number of steps
-    const totalSteps = 6;
-    return totalsteps - index - 1;
-}
+const reverseIndex = (index) => {
+  // Number of steps
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+};
 
 const Stairs = () => {
-  return 
+  return (
     <>
-        {/* 
+      {/* 
         Render 6 motion divs, each representing a step of  the stairs.
         Each div will have the same animation defined by the stairAnimation object.
         The delay for each div is calculated sinamically based on it's reversed index,
         creating a staggered effect with decreasing delay for each subsequent steps.
          */}
+      {[...Array(6)].map((_, index) => {
+        return (
+        <motion.div
+          key={index}
+          variants={stairAnimation}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: reverseIndex(index) * 0.1,
+          }}
+          className="h-full w-full bg-accent relative"
+        />
+        );
+      })}
     </>
-}
+  );
+};
 
-export default Stairs
+export default Stairs;
