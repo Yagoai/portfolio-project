2926815dc04cd787b644138c90ff9dac00bbc4fc
NguyenHongAnh <nguyenhonganh19052003@gmail.com>
2026-02-11 17:01:32 +0700

    update: re-design layout

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full relative flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{
@@ -19,7 +19,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten relative"
         >
           <Image
             src="/assets/photo.jpg"
