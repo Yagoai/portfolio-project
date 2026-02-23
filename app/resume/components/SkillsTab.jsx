99be1a5a42a1f2c29b080eb03df133b6a1a983c4
NguyenHongAnh <nguyenhonganh19052003@gmail.com>
2026-02-24 01:02:49 +0700

    update: update SkillsTab

---
+++ app/resume/components/SkillsTab.jsx
@@ -0,0 +1,43 @@
+import { TabsContent } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export default function SkillsTab({ data }) {
+  return (
+    <TabsContent value="skills" className="w-full h-full">
+      <div className="flex flex-col gap-[30px]">
+        <div className="flex flex-col gap-[30px] text-center xl:text-left">
+          <h3 className="text-4xl font-bold">{data.title}</h3>
+          <p className="max-w-[600px] text-white/60 mx:auto xl:mx-0">
+            {data.description}
+          </p>
+        </div>
+
+        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+          {data.skillList.map((skill, index) => {
+            return (
+              <li key={index}>
+                <TooltipProvider delayDuration={100}>
+                  <Tooltip>
+                    <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                      <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                        {skill.icon}
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p className="capitalize">{skill.name}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </TabsContent>
+  );
+}
